16fb2096b8ad9080255febac7a4953b9f162a41d
apparao <apparao123.g@gmail.com>
2020-02-24 19:49:55 +0530

    call back Refs

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -3,14 +3,22 @@ import React, { Component } from "react";
 class RefsDemo extends Component {
   constructor(props) {
     super(props);
+
     this.inputRef = React.createRef();
+    this.cbRef = null;
+    //the below code pass the DOM element as parameter
+    this.setCbRef = element => {
+      this.cbRef = element;
+    };
   }
   componentDidMount() {
     console.log("in componentDidMount");
-    this.inputRef.current.focus();
-    console.log(this.inputRef);
+    // this.inputRef.current.focus();
+    if (this.cbRef) {
+      this.cbRef.focus();
+    }
+    // console.log(this.inputRef);
   }
-
   clickHandler = () => {
     alert(this.inputRef.current.value);
   };
@@ -19,6 +27,7 @@ class RefsDemo extends Component {
     return (
       <div>
         <input type='text' ref={this.inputRef} />
+        <input type='text' ref={this.cbRef} />
         <button onClick={this.clickHandler}>Submit</button>
       </div>
     );
