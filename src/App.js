123b9e857f83853b4a77a6d1423c461615692f1f
apparao <apparao123.g@gmail.com>
2020-02-24 19:54:39 +0530

    Ref with Class compoents

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 class App extends Component {
   render() {
-    return <div className='App'>{<RefsDemo />}</div>;
+    return <div className='App'>{<FocusInput />}</div>;
   }
 }
 
