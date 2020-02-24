08eb58a2d8ecaeff6d3841a642a9ce4ce9bb977d
apparao <apparao123.g@gmail.com>
2020-02-24 19:29:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+class App extends Component {
+  render() {
+    // return <div className='App'>{<FRParentInput />}</div>;
+    return (
+      <div className='App'>
+        <ErrorBoundary>
+          <Hero heroName='Rama' />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName='krishna' />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName='Joker' />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
