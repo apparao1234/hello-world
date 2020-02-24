e53470c9aa1ba46d8f95e88c36869f3df98179b0
apparao <apparao123.g@gmail.com>
2020-02-24 20:30:04 +0530

    React Fragments approach2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import FragmentDemo from "./components/FragmentDemo";
+import FragmentDemoApproach2 from "./components/FragmentDemoApproach2";
 
 class App extends Component {
   render() {
-    return <div className='App'>{<FragmentDemo />}</div>;
+    return <div className='App'>{<FragmentDemoApproach2 />}</div>;
   }
 }
 
---
+++ src/components/FragmentDemoApproach2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FragmentDemoApproach2() {
+  // the below <> explains to avoid
+  return (
+    <>
+      <h1>Fragment Demo approach2</h1>
+      <p>This describes the fragment Demo</p>
+    </>
+  );
+}
+
+export default FragmentDemoApproach2;
