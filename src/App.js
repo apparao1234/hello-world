150aa0c7882aefe34d672d9bdd335744df778009
apparao <apparao123.g@gmail.com>
2020-02-24 20:04:47 +0530

    Mounting Life cycle methods

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import FRParentInput from "./components/FRParentInput";
+import LifeCycleA from "./components/LifeCycleA";
 
 class App extends Component {
   render() {
-    return <div className='App'>{<FRParentInput />}</div>;
+    return <div className='App'>{<LifeCycleA />}</div>;
   }
 }
 
