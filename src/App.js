9ccce199f348bedb6b53a18e92247b72223852c2
apparao <apparao123.g@gmail.com>
2020-02-26 20:54:47 +0530

    props with class compoent

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Greet name='Ram' heroName='Rama'>
-          <p>This is children props</p>
-        </Greet>
-        <Greet name='Hanu' heroName='Hanumonth'>
-          <button>Action</button>
-        </Greet>
-        <Greet name='Arujn' heroName='Krishna' />
+        <Welcome name='Ram' heroName='Rama' />
+
+        <Welcome name='Hanu' heroName='Hanumonth'></Welcome>
+        <Welcome name='Arujn' heroName='Krishna' />
       </div>
     );
   }
