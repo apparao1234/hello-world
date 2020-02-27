d1e2dac7928a817c4a9b9cf990fa59e35ff567fc
apparao <apparao123.g@gmail.com>
2020-02-27 13:29:13 +0530

    using context type with out using Context consumer

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import UserContext from "./userContext";
 
 class ComponentE extends Component {
   render() {
     return (
       <div>
+        ComponentE Context {this.context}
         <ComponentF />
       </div>
     );
   }
 }
-
+ComponentE.contextType = UserContext;
 export default ComponentE;
